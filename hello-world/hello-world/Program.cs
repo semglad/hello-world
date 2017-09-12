5f51af04a3d298894d07dfe8c300d806be9e8cb9
Sem Glad <sem.glad@student.saimia.fi>
2017-09-12 10:23:18 +0300

    Created basic hello world example

---
+++ hello-world/hello-world/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace hello_world
+{
+    class Program
+    {
+        static void Main(string[] args) //method
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Press Any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
